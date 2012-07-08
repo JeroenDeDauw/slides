6148f3beb4d9bbd273831e10ecf5348f2bcb9624
jeroendedauw <jeroendedauw@gmail.com>
2012-07-08 17:41:02 +0200

    fix laggs

--- wikimania/dc/resources/index.js
+++ wikimania/dc/resources/index.js
@@ -14,8 +14,6 @@ Dz.init = function() {
 
 	this.doLoop = function() {
 		_this.view = _this.iframe.contentWindow;
-		_this.postMsg(_this.view, "REGISTER");
-
 		setTimeout( function() { _this.doLoop(); }, 500 );
 	};
 
